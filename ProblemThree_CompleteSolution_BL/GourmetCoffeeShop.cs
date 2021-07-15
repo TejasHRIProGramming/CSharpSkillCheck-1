6a0cf89ae01a594b09aa688b512b10bec2e7ddbd
TejasHRIProGramming <tejashri.agh@ltts.com>
2021-07-15 18:00:08 +0530

    Third commit

--- ProblemThree_CompleteSolution_BL/GourmetCoffeeShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetCoffeeShop.cs
@@ -8,6 +8,32 @@ namespace ProblemThree_CompleteSolution_BL
 {
     public class GourmetCoffeeShop : GourmetShop, ITaxCompute
     {
-        
+        private int v1;
+        private string itemName;
+        private Dictionary<int, double> coffeePriceDict;
+
+        public GourmetCoffeeShop(int v1, string itemName, Dictionary<int, double> coffeePriceDict)
+        {
+            this.v1 = v1;
+            this.itemName = itemName;
+            this.coffeePriceDict = coffeePriceDict;
+        }
+
+        public int ItemName { get; set; }
+
+        public override double PayPerPiece(int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double PayPerWeight(int weight)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double TaxOnTotalBill()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
@@ -6,8 +6,35 @@ using System.Threading.Tasks;
 
 namespace ProblemThree_CompleteSolution_BL
 {
-    public class GourmetOrganicShop : GourmetShop,ITaxCompute
+    public class GourmetOrganicShop : GourmetShop, ITaxCompute
     {
-        
+        private int v1;
+        private string itemName;
+        private Dictionary<int, double> organicPriceDict;
+
+        public GourmetOrganicShop(int v1, string itemName, Dictionary<int, double> organicPriceDict)
+        {
+            this.v1 = v1;
+            this.itemName = itemName;
+            this.organicPriceDict = organicPriceDict;
+        }
+
+        public int ItemName { get; set; }
+
+        public override double PayPerPiece(int quantity)
+        {
+            throw new NotImplementedException();
+            
+        }
+
+        public override double PayPerWeight(int weight)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double TaxOnTotalBill()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
